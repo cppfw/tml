72b5281d6d17a4d0238f078d8dc6a971946aa08d
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-01 14:55:24 +0000

    test file

--- makefile
+++ makefile
@@ -13,12 +13,4 @@ endif
 
 
 include ./targets.mk
-
-$(possible_targets_list):
-	@$(MAKE) $(subdirs) target=$@
-
-.PHONY: $(subdirs)
-$(subdirs):
-	@$(MAKE) -C $@ $(target)
-
-
+include ./subdirs.mk
---
+++ tests/basic/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ivan
+ *
+ * Created on July 1, 2012, 5:54 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
