6090922bd06cacbff71d8642ef4b4fde00338340
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-01 21:29:17 +0000

    stuff

--- tests/basic/main.cpp
+++ tests/basic/main.cpp
@@ -1,6 +1,7 @@
 #include "../../src/stob.hpp"
 
 #include <string>
+#include <deque>
 
 #include <ting/debug.hpp>
 #include <ting/fs/FSFile.hpp>
@@ -9,35 +10,52 @@
 
 namespace TestSimpleQuotedStringParsing{
 
+enum E_Action{
+	CHILDREN_START,
+	CHILDREN_END,
+	STRING
+};
+
 class Listener : public stob::ParseListener{
 	//override
 	void OnChildrenParseFinished(){
-		ASSERT_ALWAYS(false)
+		ASSERT_ALWAYS(this->actions.size() > 0)
+		ASSERT_INFO_ALWAYS(this->actions.front().first == CHILDREN_END, "first = " << this->actions.front().first << " second = " << this->actions.front().second)
+		this->actions.pop_front();
 	}
 	
 	//override
 	void OnChildrenParseStarted(){
-		ASSERT_ALWAYS(false)
+		ASSERT_ALWAYS(this->actions.size() > 0)
+		ASSERT_INFO_ALWAYS(this->actions.front().first == CHILDREN_START, "first = " << this->actions.front().first << " second = " << this->actions.front().second)
+		this->actions.pop_front();
 	}
 	
 	//override
 	void OnStringParsed(const char* s, ting::u32 size){
-		ASSERT_INFO_ALWAYS(std::string(s, size) == "test string", "str = " << std::string(s, size))
-		this->called = true;
+		ASSERT_ALWAYS(this->actions.size() > 0)
+		ASSERT_INFO_ALWAYS(this->actions.front().first == STRING, "first = " << this->actions.front().first << " second = " << this->actions.front().second)
+		ASSERT_INFO_ALWAYS(this->actions.front().second == std::string(s, size), "first = " << this->actions.front().first << " second = " << this->actions.front().second << " str = " << std::string(s, size))
+		this->actions.pop_front();
 	}
 	
 public:
-	ting::Inited<bool, false> called;
+	
+	std::deque<std::pair<E_Action, std::string> > actions;
 };
 
 void Run(){
 	Listener l;
 	
-	ting::fs::FSFile fi("test1.stob");
+	typedef std::pair<E_Action, std::string> T_Pair;
+	l.actions.push_back(T_Pair(STRING, "test string"));
+	l.actions.push_back(T_Pair(STRING, "another string"));
+	
+	ting::fs::FSFile fi("test.stob");
 	
 	stob::Parse(fi, l);
 	
-	ASSERT_ALWAYS(l.called)
+	ASSERT_ALWAYS(l.actions.size() == 0)
 }
 
 }
