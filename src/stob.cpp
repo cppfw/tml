f16683a43b76abe7b2ae0f7bcad60e7e680e34dc
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-01 17:53:16 +0000

    some stuff

--- src/stob.cpp
+++ src/stob.cpp
@@ -11,10 +11,65 @@ using namespace stob;
 
 
 
+void Parser::ParseChar(ting::u8 c, ParseListener& listener){
+	switch(this->state){
+		case IDLE:
+			switch(c){
+				case '"':
+					this->state = QUOTED_STRING;
+					break;
+				case '{':
+					if(this->nestingLevel == unsigned(-1)){
+						throw stob::Exc("Malformed STOB document. Nesting level is too high.");
+					}
+					if(!this->stringParsed){
+						throw stob::Exc("Malformed STOB document. Curly braces without preceding string declaration encountered.");
+					}
+					++this->nestingLevel;
+					this->stringParsed = false;
+					listener.OnChildrenParseStarted();
+					break;
+				case '}':
+					if(this->nestingLevel == 0){
+						std::stringstream ss;
+						ss << "Malformed STOB document. Unexpected '}' at line: ";
+						ss << this->curLine;
+						throw stob::Exc(ss.str());
+					}
+					--this->nestingLevel;
+					listener.OnChildrenParseFinished();
+					break;
+				case '\n':
+					++this->curLine;
+					break;
+				case '\r':
+				case ' ':
+				case '\t':
+					//ignore
+					break;
+				default:
+					this->state = UNQUOTED_STRING;
+					break;
+			}
+			break;
+		case QUOTED_STRING:
+			//TODO:
+			break;
+		case UNQUOTED_STRING:
+			//TODO:
+			break;
+		default:
+			ASSERT(false)
+			break;
+	}
+}
+
+
+
 void Parser::ParseDataChunk(const ting::Buffer<ting::u8>& chunk, ParseListener& listener){
 	for(ting::u8* s = chunk.Begin(); s != chunk.End(); ++s){
-		//TODO:
-		if(this->commentSeqenceStarted){
+//		if(this->prevChar != 0){
+		if(this->prevChar == '/'){//check if possible comment sequence
 			ASSERT(this->commentState == NO_COMMENT)
 			switch(*s){
 				case '/':
@@ -29,7 +84,6 @@ void Parser::ParseDataChunk(const ting::Buffer<ting::u8>& chunk, ParseListener& 
 					//do nothing
 					break;
 			}
-			this->commentSeqenceStarted = false;
 			
 			//skip comments if needed
 			if(this->commentState != NO_COMMENT){
@@ -64,57 +118,7 @@ void Parser::ParseDataChunk(const ting::Buffer<ting::u8>& chunk, ParseListener& 
 			continue;
 		}
 		
-		switch(this->state){
-			case IDLE:
-				switch(*s){
-					case '"':
-						this->state = QUOTED_STRING;
-						break;
-					case '{':
-						if(this->nestingLevel == unsigned(-1)){
-							throw stob::Exc("Malformed STOB document. Nesting level is too high.");
-						}
-						if(!this->stringParsed){
-							throw stob::Exc("Malformed STOB document. Curly braces without preceding string declaration encountered.");
-						}
-						++this->nestingLevel;
-						this->stringParsed = false;
-						listener.OnChildrenParseStarted();
-						break;
-					case '}':
-						if(this->nestingLevel == 0){
-							std::stringstream ss;
-							ss << "Malformed STOB document. Unexpected '}' at line: ";
-							ss << this->curLine;
-							throw stob::Exc(ss.str());
-						}
-						break;
-					case '\n':
-						++this->curLine;
-						break;
-					case '\r':
-					case ' ':
-					case '\t':
-						//ignore
-						break;
-					case '/':
-						this->commentSeqenceStarted = true;
-						break;
-					default:
-						this->state = UNQUOTED_STRING;
-						break;
-				}
-				break;
-			case QUOTED_STRING:
-				//TODO:
-				break;
-			case UNQUOTED_STRING:
-				//TODO:
-				break;
-			default:
-				ASSERT(false)
-				break;
-		}
+		//TODO:
 	}
 }
 
