1c2bc45582590cf6240dae652945d668da2ff8dc
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-02 12:23:14 +0000

    stuff

--- src/stob/dom.cpp
+++ src/stob/dom.cpp
@@ -8,6 +8,26 @@ using namespace stob;
 
 
 
+namespace{
+ting::MemoryPool<sizeof(Node), 4096 / sizeof(Node)> memoryPool;
+}
+
+
+
+void* Node::operator new(size_t size){
+	ASSERT(size == sizeof(Node))
+
+	return memoryPool.Alloc_ts();
+}
+
+
+
+void Node::operator delete(void* p)throw(){
+	memoryPool.Free_ts(p);
+}
+
+
+
 ting::Ptr<stob::Node> Load(ting::fs::File& fi){
 	//TODO:
 	
