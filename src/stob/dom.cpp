91066dfcfc52e0f2a497a60f24d040dbb3c9537e
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-02 12:05:54 +0000

    stuff

---
+++ src/stob/dom.cpp
@@ -0,0 +1,15 @@
+#include "dom.hpp"
+
+#include <ting/debug.hpp>
+
+
+
+using namespace stob;
+
+
+
+ting::Ptr<stob::Node> Load(ting::fs::File& fi){
+	//TODO:
+	
+	return ting::Ptr<stob::Node>();
+}
