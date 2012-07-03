61711ed58af16d9e239b323203e6a1292fa31cac
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-03 12:51:55 +0000

    stuff

--- trunk/src/stob/dom.cpp
+++ trunk/src/stob/dom.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 
 #include <ting/debug.hpp>
+#include <ting/Array.hpp>
 
 #include "parser.hpp"
 
@@ -44,6 +45,12 @@ Node* Node::Next(const std::string& value)throw(){
 
 
 
+const Node* Node::Next(const std::string& value)const throw(){
+	return const_cast<Node const*>(this)->Next(value);
+}
+
+
+
 Node* Node::Child(const std::string& value)throw(){
 	if(this->children.IsNotValid()){
 		return 0;
@@ -58,8 +65,178 @@ Node* Node::Child(const std::string& value)throw(){
 
 
 
-void Node::Write(ting::fs::File& fi){
-	//TODO:
+const Node* Node::Child(const std::string& value)const throw(){
+	return const_cast<Node const*>(this)->Child(value);
+}
+
+
+
+namespace{
+
+bool CanStringBeUnquoted(const std::string& s, unsigned& numEscapes){
+	numEscapes = 0;
+	bool ret = true;
+	for(const char* c = s.c_str(); c != 0; ++c){
+		switch(*c){
+			case '\t':
+			case '\n':
+			case '\r':
+			case '"':
+				++numEscapes;
+			case '{':
+			case '}':
+			case ' ':
+				ret = false;
+				break;
+			default:
+				break;
+		}
+	}
+	return ret;
+}
+
+
+void MakeEscapedString(const std::string& str, ting::Buffer<ting::u8>& out){
+	ting::u8 *p = out.Begin();
+	for(const char* c = str.c_str(); c != 0; ++c){
+		ASSERT(p != out.End())
+		
+		switch(*c){
+			case '\t':
+				*p = '\\';
+				++p;
+				ASSERT(p != out.End())
+				*p = 't';
+				break;
+			case '\n':
+				*p = '\\';
+				++p;
+				ASSERT(p != out.End())
+				*p = 'n';
+				break;
+			case '\r':
+				*p = '\\';
+				++p;
+				ASSERT(p != out.End())
+				*p = 'r';
+				break;
+			case '"':
+				*p = '\\';
+				++p;
+				ASSERT(p != out.End())
+				*p = '"';
+				break;
+			default:
+				*p = *c;
+				break;
+		}
+		++p;
+	}
+}
+
+
+void WriteNode(const stob::Node* node, ting::fs::File& fi, bool formatted, unsigned indentation){
+	ASSERT(node)
+	
+	ting::StaticBuffer<ting::u8, 1> quote;
+	quote[0] = '"';
+	
+	ting::StaticBuffer<ting::u8, 1> lcurly;
+	quote[0] = '{';
+	
+	ting::StaticBuffer<ting::u8, 1> rcurly;
+	quote[0] = '}';
+	
+	ting::StaticBuffer<ting::u8, 1> space;
+	quote[0] = ' ';
+	
+	ting::StaticBuffer<ting::u8, 1> tab;
+	quote[0] = '\t';
+	
+	ting::StaticBuffer<ting::u8, 1> newLine;
+	quote[0] = '\n';
+	
+	
+	for(const Node* n = node->Children(); n; n = n->Next()){
+		//indent
+		for(unsigned i = 0; i != indentation; ++i){
+			fi.Write(tab);
+		}
+		
+		//write node value
+		
+		unsigned numEscapes;
+		bool unqouted = CanStringBeUnquoted(n->Value(), numEscapes);
+		
+		if(!unqouted){
+			fi.Write(quote);
+			
+			if(numEscapes == 0){
+				fi.Write(ting::Buffer<ting::u8>(
+						const_cast<ting::u8*>(reinterpret_cast<const ting::u8*>(n->Value().c_str())),
+						n->Value().size()
+					));
+			}else{
+				ting::Array<ting::u8> buf(n->Value().size() + numEscapes);
+				
+				MakeEscapedString(n->Value(), buf);
+				
+				fi.Write(buf);
+			}
+			
+			fi.Write(quote);
+		}else{
+			ASSERT(numEscapes == 0)
+			fi.Write(ting::Buffer<ting::u8>(
+					const_cast<ting::u8*>(reinterpret_cast<const ting::u8*>(n->Value().c_str())),
+					n->Value().size()
+				));
+		}
+		
+		if(n->Children() == 0){
+			if(formatted){
+				fi.Write(newLine);
+			}
+			continue;
+		}
+		
+		if(!formatted){
+			fi.Write(lcurly);
+			
+			WriteNode(n, fi, false, 0);
+			
+			fi.Write(rcurly);
+		}else{
+			if(n->Children()->Next() == 0 && n->Children()->Children() == 0){
+				//if only one child and that child has no children
+				fi.Write(space);
+				fi.Write(lcurly);
+				WriteNode(n, fi, false, 0);
+				fi.Write(rcurly);
+				fi.Write(newLine);
+			}else{
+				fi.Write(lcurly);
+				fi.Write(newLine);
+				WriteNode(n, fi, true, indentation + 1);
+				
+				//indent
+				for(unsigned i = 0; i != indentation; ++i){
+					fi.Write(tab);
+				}
+				fi.Write(rcurly);
+				fi.Write(newLine);
+			}
+		}
+	}//~for()
+}
+}//~namespace
+
+
+
+void Node::Write(ting::fs::File& fi, bool formatted){
+	ting::fs::File::Guard fileGuard(fi, ting::fs::File::CREATE);
+	
+	WriteNode(this, fi, formatted, 0);
 }
 
 
