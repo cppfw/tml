ac984e5c6267dea0b644b6a7aa51af9f9ece3380
igagis <igagis@27fd902c-a271-0fb5-2e2a-808adbbba862>
2012-07-04 12:04:51 +0000

    some comments

--- trunk/src/stob/Exc.hpp
+++ trunk/src/stob/Exc.hpp
@@ -20,6 +20,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE. */
 
+// Home page: http://stob.googlecode.com
+
+/**
+ * @author Ivan Gagis <igagis@gmail.com>
+ */
+
 #pragma once
 
 
