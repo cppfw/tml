8b8356d7cdbabb4476a98187ecfd0d33bf9fb7c4
Ivan Gagis <igagis@gmail.com>
2015-12-25 02:33:56 +0200

    ios

---
+++ ios/tests/main.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  tests
+//
+//  Created by Ivan Gagis on 25/12/15.
+//  Copyright © 2015 Ivan Gagis. All rights reserved.
+//
+
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
