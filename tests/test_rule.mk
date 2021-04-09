71d02a5951eb70a5b9c63e020f743f9c00a61ee3
Ivan Gagis <igagis@gmail.com>
2021-04-09 11:30:19 +0300

    add configs

--- tests/test_rule.mk
+++ tests/test_rule.mk
@@ -1,14 +1,13 @@
 ifeq ($(os),windows)
-    this_test_cmd := (cd $(d); cp ../../src/build/libtreeml.dll .; $$^)
+    this_test_cmd := (cd $(d); cp ../../src/out/$(c)/libtreeml.dll out/$(c); $$^)
 else
-    this_test_cmd := (cd $(d); LD_LIBRARY_PATH=../../src/build $$^)
+    this_test_cmd := (cd $(d); LD_LIBRARY_PATH=../../src/out/$(c) $$^)
 endif
-
 
 define this_rules
 test:: $(prorab_this_name)
 $(.RECIPEPREFIX)@myci-running-test.sh $$(notdir $$(abspath $$(dir $$^)))...
-$(.RECIPEPREFIX)$(Q)$(this_test_cmd)
+$(.RECIPEPREFIX)$(a)$(this_test_cmd)
 $(.RECIPEPREFIX)@myci-passed.sh
 endef
 $(eval $(this_rules))
