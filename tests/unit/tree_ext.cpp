26218bf03995b857d36423d544a9dbc6758bdc40
Ivan Gagis <igagis@gmail.com>
2021-05-04 17:26:57 +0300

    test tree_ext std::find

--- tests/unit/tree_ext.cpp
+++ tests/unit/tree_ext.cpp
@@ -21,7 +21,12 @@ tst::set set0("tree_ext", [](auto& suite){
 
     suite.add("find_tree_from_forest_by_string", [](){
         std::string str = "hello world!";
-        treeml::forest_ext f{{"hi"}, {"how are you?"}, {str}, {"I'm fine"}};
+        treeml::forest_ext f{
+            {"hi"},
+            {"how are you?"},
+            {str},
+            {"I'm fine"},
+        };
 
         auto i = std::find(f.begin(), f.end(), str.c_str());
         tst::check(i != f.end(), SL);
