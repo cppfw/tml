e1f23ddd08a8540d41b27ebde65ad5fa8f2f6f84
Ivan Gagis <igagis@gmail.com>
2021-05-06 01:14:06 +0300

    comment list dir code

--- tests/unit/src/tree_writing.cpp
+++ tests/unit/src/tree_writing.cpp
@@ -37,32 +37,30 @@ std::function<void(const std::string&)> make_test_proc(){
 
 namespace{
 tst::set set("tree_writing", [](auto& suite){
-	std::vector<std::string> files;
+	std::vector<std::string> files = {"sample1.tml", "sample2.tml"};
 
-	{
-		const std::regex tml_regex("^.*\\.tml$");
+	// TODO: re-enable when problem in qemu is fixed, see https://bugs.launchpad.net/qemu/+bug/1805913
+	// {
+	// 	const std::regex tml_regex("^.*\\.tml$");
+	// 	auto all_files = papki::fs_file(data_dir).list_dir();
 
-		auto all_files = papki::fs_file(data_dir).list_dir();
+	// 	std::copy_if(
+	// 			all_files.begin(),
+	// 			all_files.end(),
+	// 			std::back_inserter(files),
+	// 			[&tml_regex](auto& f){
+	// 				return std::regex_match(f, tml_regex);
+	// 			}
+	// 		);
+	// }
 
-		std::copy_if(
-				all_files.begin(),
-				all_files.end(),
-				std::back_inserter(files),
-				[&tml_regex](auto& f){
-					return std::regex_match(f, tml_regex);
-				}
-			);
-	}
-
-	// TODO: re-eanble when problem in qemu is ifxed, see https://bugs.launchpad.net/qemu/+bug/1805913
-	suite.template add_disabled<std::string>(
+	suite.template add<std::string>(
 		"write_unformatted",
 		decltype(files)(files),
 		make_test_proc<false>()
 	);
 
-	// TODO: re-eanble when problem in qemu is ifxed, see https://bugs.launchpad.net/qemu/+bug/1805913
-	suite.template add_disabled<std::string>(
+	suite.template add<std::string>(
 		"write_formatted",
 		decltype(files)(files),
 		make_test_proc<true>()
