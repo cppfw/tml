3d4b2b9ebf0ba951d854911e1e2800f1a0781a53
Ivan Gagis <igagis@gmail.com>
2024-12-04 10:39:53 +0200

    conan

---
+++ conan/test_package/example.cpp
@@ -0,0 +1,13 @@
+#include <tml/tree.hpp>
+
+#include <iostream>
+
+using namespace std::string_literals;
+
+int main(int argc, const char** argv){
+	tml::tree t("hello"s, {"tml!"s});
+
+	std::cout << "tml = " << tml::to_string(t) << std::endl;
+
+	return 0;
+}
